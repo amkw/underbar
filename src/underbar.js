2068eb8f831a352e0700e67d439d60ae216637ab
AlyssaWei <wei.alyssa@gmail.com>
2019-03-21 12:56:08 -0700

    Added function body to _.reject()

--- src/underbar.js
+++ src/underbar.js
@@ -97,6 +97,10 @@
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    // test is a function that returns a boolean
+    return _.filter(collection, function(item) {
+      return !test(item);
+    });
   };
 
   // Produce a duplicate-free version of the array.
